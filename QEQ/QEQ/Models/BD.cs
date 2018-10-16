9a7b21b74a0f24e3db5cca37dfcab6b4dea693e4
CANDELA KRUMMER MATEOS <43725125@ort>
2018-10-16 12:05:29 -0300

    abm Categorias: 50%

---
+++ QEQ/QEQ/Models/BD.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace QEQ.Models
+{
+    public static class BD
+    {
+        public static string ConectionString = "Server=10.128.8.16;DataBase=QEQC03;User ID=QEQC03;Pwd=QEQC03;";
+
+        public static SqlConnection Conectar()
+        {
+            SqlConnection Conexion = new SqlConnection(ConectionString);
+            Conexion.Open();
+            return Conexion;
+        }
+
+        public static void Desconectar(SqlConnection Conexion)
+        {
+            Conexion.Close();
+        }
+
+        public static List<Categorias> ListarCategorias(int id = 0)
+        {
+            List<Categorias> ListaDeCategorias = new List<Categorias>();
+            SqlConnection Conexion = Conectar();
+            SqlCommand Consulta = Conexion.CreateCommand();
+            Consulta.CommandText = "ListarCategorias";
+            Consulta.CommandType = System.Data.CommandType.StoredProcedure;
+            Consulta.Parameters.AddWithValue("@id", id);
+            SqlDataReader DataReader = Consulta.ExecuteReader();
+            while (DataReader.Read())
+            {
+                int Id = Convert.ToInt32(DataReader["id"]);
+                string Nombre = DataReader["Nombre"].ToString();
+                
+
+                Categorias cate = new Categorias(Id, Nombre);
+                ListaDeCategorias.Add(cate);
+
+            }
+            Desconectar(Conexion);
+            return ListaDeCategorias;
+        }
+
+        public static void InsertarCategoria(string Nombre)
+        {
+            SqlConnection Conexion = Conectar();
+            SqlCommand Consulta = Conexion.CreateCommand();
+            Consulta.CommandText = "InsertarCategoria";
+            Consulta.CommandType = System.Data.CommandType.StoredProcedure;
+            Consulta.Parameters.AddWithValue("@Nombre", Nombre);
+            Consulta.ExecuteNonQuery();
+
+            Desconectar(Conexion);
+        }
+
+        public static void EliminarCategoria(int id)
+        {
+            SqlConnection Conexion = Conectar();
+            SqlCommand Consulta = Conexion.CreateCommand();
+            Consulta.CommandText = "EliminarCategoria";
+            Consulta.CommandType = System.Data.CommandType.StoredProcedure;
+            Consulta.Parameters.AddWithValue("@id", id);
+            Consulta.ExecuteNonQuery();
+
+            Desconectar(Conexion);
+        }
+
+        public static void ModificarCategoria(Categorias cate)
+        {
+            SqlConnection Conexion = Conectar();
+            SqlCommand Consulta = Conexion.CreateCommand();
+            Consulta.CommandText = "ModificarCategoria";
+            Consulta.CommandType = System.Data.CommandType.StoredProcedure;
+            Consulta.Parameters.AddWithValue("@Nombre", cate.Nombre);
+            Consulta.Parameters.AddWithValue("@id", cate.id);
+            Consulta.ExecuteNonQuery();
+
+            Desconectar(Conexion);
+        }
+    }
+}
---
+++ QEQ/QEQ/Models/Categorias.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace QEQ.Models
+{
+    public class Categorias
+    {
+        private int _id;
+        private string _Nombre;
+
+        public Categorias()
+        {
+
+        }
+
+        public Categorias(int id, string Nombre)
+        {
+            _id = id;
+            _Nombre = Nombre;
+        }
+
+        public int id { get => _id; set => _id = value; }
+
+        [Required(ErrorMessage = "El campo no puede estar vacío")]
+        [Range(0, 200, ErrorMessage = "El nombre no puede sobrepasar los 200 caracteres")]
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+    }
+}
