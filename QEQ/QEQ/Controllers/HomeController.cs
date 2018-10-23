08a4ad5e4b7a378d468633c655d925f7dc8b530a
Nicolas Lerman <nicomlerman@outlook.com>
2018-10-23 10:48:20 -0300

    MENU ARREGLADO

--- QEQ/QEQ/Controllers/HomeController.cs
+++ QEQ/QEQ/Controllers/HomeController.cs
@@ -13,21 +13,7 @@ namespace QEQ.Controllers
         {
             return View();
         }
-        /*
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-        */
+        
         public ActionResult Login()
         {
             return View();
